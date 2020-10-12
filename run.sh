eea7b6737ae0de6fe8c7e4187db29c369031042e
Greg Brisebois <gregoryjjb@gmail.com>
2020-10-12 02:23:32 +0000

    pacific time

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-xvfb-run node ./main.js | while IFS= read -r line; do printf '%s %s\n' "[$(date)]" "$line"; done 2>&1 | tee -a cache/output.log  # cache/$(date +%Y-%m-%d).log
+xvfb-run node ./main.js | while IFS= read -r line; do printf '%s %s\n' "[$(TZ=America/Los_Angeles date)]" "$line"; done 2>&1 | tee -a cache/output.log  # cache/$(date +%Y-%m-%d).log
